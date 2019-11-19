fe9362ddcc769a6a437ca8104538d0a71a46d42a
abdalmounaim <abdalmounaim@gmail.com>
2019-11-19 09:11:45 +0100

    First commit: Strategy Pattern

---
+++ animal/Giraffe.java
@@ -0,0 +1,38 @@
+public class Giraffe extends Creature{
+
+	private String name;
+	private double height;
+	private double weight;
+	private String favFood;
+	private double speed;
+	private String sound;
+	
+	public void setName(String newName){ name = newName; }
+	public String getName(){ return name; }
+	
+	public void setHeight(double newHeight){ height = newHeight; }
+	public double getHeight(){ return height; }
+	
+	public void setWeight(int newWeight){ 
+		if (newWeight > 0){
+			weight = newWeight; 
+		} else {
+			System.out.println("Weight must be bigger than 0");
+		}
+	}
+	public double getWeight(){ return weight; }
+	public void setWeight(double weight){
+		this.weight = weight;
+	}
+	
+	public void setFavFood(String newFavFood){ favFood = newFavFood; }
+	public String getFavFood(){ return favFood; }
+	
+	public void setSpeed(double newSpeed){ speed = newSpeed; }
+	public double getSpeed(){ return speed; }
+	
+	public void setSound(String newSound){ sound = newSound; }
+	public String getSound(){ return sound; }
+	
+	
+}
---
+++ animal/Living.java
@@ -0,0 +1,56 @@
+01
+public class Living extends Creature{
+02
+ 
+03
+    private String name;
+04
+     
+05
+    @Override
+06
+    public void setName(String newName) {
+07
+        name = newName;
+08
+         
+09
+    }
+10
+ 
+11
+    @Override
+12
+    public String getName() {
+13
+        // TODO Auto-generated method stub
+14
+        return name;
+15
+    }
+16
+ 
+17
+    @Override
+18
+    public void setWeight(double newWeight) {
+19
+        // TODO Auto-generated method stub
+20
+         
+21
+    }
+22
+ 
+23
+    @Override
+24
+    public double getWeight() {
+25
+        // TODO Auto-generated method stub
+26
+        return 0;
+27
+    }
+28
+}
